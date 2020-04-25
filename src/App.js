161ec8d4bb86eebc5360f6d610e9ecf9eea296ae
aman mirza <aman mirza99>
2020-04-26 04:50:29 +0500

    E-commerce website making (Forms and component) part-4 shop data urls and images

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Route, Link } from "react-router-dom";
-// import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from "react-router-dom";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HomePage = (props) => {
-  // ager yahan props dain gy to ye route / ky sath jo bhi define hoga wo bi dega
-  console.log(props);
-  return (
-    <div>
-      {/* <Link to="/topics">topics</Link> */}
-      <button onClick={()=>props.history.push('/topics')}>Topics</button>
-      <h1>HomePage PAGE</h1>
-    </div>
-  );
-};
-const TopicList = (props) => {
-  console.log(props);
 
-  return (
-    <div>
-      <h1>Topic List Details</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/19`}>TO TOPIC 19</Link>
-      <Link to={`${props.match.url}/99`}>TO TOPIC 99</Link>
-
-    </div>
-  );
-};
-const TopicDetail = (props) => {
-  console.log(props);
-
-  return (
-    <div>
-      <h1>Topic detail Page: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
 function App() {
   return (
     <div>
-      {/* <Switch> */}
+      <Switch>
       <Route path="/" component={HomePage} />
-      <Route path="/topics" component={TopicList} />
-      <Route path="topics/:topicId" component={TopicDetail} />
-      {/* </Switch> */}
+      <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
