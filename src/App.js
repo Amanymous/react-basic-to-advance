8cfc7d0e5f5bb51e95b1ff6140c59622661211fb
aman mirza <aman mirza99>
2020-04-27 00:55:32 +0500

    E-commerce website making (Forms and component) part-9 firebase authentication

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import signInUp from './pages/sign-in-up/sign-in-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      currentUser:null
+    }
+  }
+  // 2nd
+  unsubscribeFromAuth=null;
+
+  componentDidMount(){
+    // 1st
+    this.unsubscribeFromAuth= auth.onAuthStateChanged(user=>{
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+  // 3rd
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
   return (
     <div>
       <Header/>
@@ -18,7 +41,7 @@ function App() {
       </Switch>
     </div>
   );
-}
+}}
 
 export default App;
 // ager exact ma dunga router ma to wo ek fix cheez ko render karega
