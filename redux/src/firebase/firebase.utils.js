86a06b399f0234ffcba33a2a2328384092676a3a
aman mirza <aman mirza99>
2020-04-30 11:57:27 +0500

    series 15 done

--- redux/src/firebase/firebase.utils.js
+++ redux/src/firebase/firebase.utils.js
@@ -17,17 +17,17 @@ firebase.initializeApp(config);
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-
+  const userRef = firestore.doc(`users/2356dakfh@`);
+  console.log(userRef)
   const snapShot = await userRef.get();
-
+   
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
     try {
       await userRef.set({
-        displayName,
-        email,
+        displayName: 'Test User',
+        email: 'random@gmail.com',
         createdAt,
         ...additionalData
       });
